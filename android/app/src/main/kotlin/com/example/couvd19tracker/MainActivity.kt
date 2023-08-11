bed547d22696539c91d3d1cad917ac9cb376ddf1
Rohitkumar1998 <rohitgupta199920@gmail.com>
2023-08-11 18:55:36 +0530

    add

---
+++ android/app/src/main/kotlin/com/example/couvd19tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.couvd19tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/word_state.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WordStateData extends StatefulWidget {
+  const WordStateData({Key? key}) : super(key: key);
+
+  @override
+  State<WordStateData> createState() => _WordStateDataState();
+}
+
+class _WordStateDataState extends State<WordStateData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('yes')),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
